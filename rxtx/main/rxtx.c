43754fc9801159583bd82c39557250529bb90836
Arnaud Dethise <arnauddethise@gmail.com>
2016-05-03 17:07:36 +0200

    changed message length ; added led to identify rxtx

--- rxtx/main/rxtx.c
+++ rxtx/main/rxtx.c
@@ -49,11 +49,10 @@
 #include <stdio.h>
 #include <string.h>
 
-
-static char *message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies amet.";
-
+static char* message = "Lorem ipsum dolor sit amet, \
+			consectetur adipiscing elit. Morbi vehicula id.";
 /*---------------------------------------------------------------------------*/
-PROCESS(example_broadcast_process, "Broadcast example");
+PROCESS(example_broadcast_process, "Broadcast communicator");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
 static void
@@ -62,7 +61,7 @@ broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
   printf("broadcast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
   		 if(strcmp((char *)packetbuf_dataptr(),message)==0)
-         leds_toggle(LEDS_YELLOW);
+         leds_toggle(LEDS_BLUE);
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
@@ -76,6 +75,7 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
   PROCESS_BEGIN();
 
   broadcast_open(&broadcast, 129, &broadcast_call);
+  leds_on(LEDS_GREEN);
 
   while(1) {
 
